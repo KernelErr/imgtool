976910d507b29d66b9593e052ac28bc075ac4080
naokilh <821874555@qq.com>
2022-01-01 21:04:09 +0800

    update test_tile

--- src/process/tile.rs
+++ src/process/tile.rs
@@ -1,8 +1,16 @@
 use anyhow::Result;
-use image::{DynamicImage, RgbaImage};
+use image::{DynamicImage, GenericImageView, RgbaImage};
 
-pub fn execute(image: DynamicImage) -> Result<DynamicImage> {
-    let mut canvas = RgbaImage::new(1920, 1080);
+pub fn execute(image: DynamicImage, width: u32, height: u32) -> Result<DynamicImage> {
+    let mut canvas = RgbaImage::new(width, height);
     image::imageops::tile(&mut canvas, &image);
-    Ok(image)
+    Ok(DynamicImage::ImageRgba8(canvas))
+}
+
+#[test]
+fn test_tile() {
+    let img = image::open("./tests/images/test_tile.jpg").unwrap();
+
+    let img = execute(img, 1920, 1080).unwrap();
+    assert_eq!(img.dimensions(), (1920, 1080));
 }
