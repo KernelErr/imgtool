89b820729590976fa57b708a89cbeaa79a5bbde2
naokilh <821874555@qq.com>
2022-01-04 15:06:59 +0800

    update code

--- src/process/tile.rs
+++ src/process/tile.rs
@@ -2,9 +2,9 @@ use anyhow::Result;
 use image::{DynamicImage, RgbaImage};
 
 // Tile an image into a new image.
-pub fn execute(image: DynamicImage, width: u32, height: u32) -> Result<DynamicImage> {
+pub fn execute(image: &DynamicImage, width: u32, height: u32) -> Result<DynamicImage> {
     let mut canvas = RgbaImage::new(width, height);
-    image::imageops::tile(&mut canvas, &image);
+    image::imageops::tile(&mut canvas, image);
     Ok(DynamicImage::ImageRgba8(canvas))
 }
 
@@ -16,8 +16,7 @@ mod tests {
     #[test]
     fn test_tile() {
         let img = image::open("./tests/images/ryan-yao-VURwPtZqyF4-unsplash.jpg").unwrap();
-
-        let img = execute(img, 1920, 1080).unwrap();
+        let img = execute(&img, 1920, 1080).unwrap();
         assert_eq!(img.dimensions(), (1920, 1080));
     }
 }
