f1dc710916dac2b1aa0bffda5f9b071c6fa2fad3
naokilh <821874555@qq.com>
2022-01-04 15:10:36 +0800

    update test

--- src/process/tile.rs
+++ src/process/tile.rs
@@ -11,11 +11,17 @@ pub fn execute(image: &DynamicImage, width: u32, height: u32) -> Result<DynamicI
 #[cfg(test)]
 mod tests {
     use self::super::*;
-    use image::GenericImageView;
+    use image::{GenericImageView, GrayImage};
 
     #[test]
-    fn test_tile() {
+    fn test_rgb_tile() {
         let img = image::open("./tests/images/ryan-yao-VURwPtZqyF4-unsplash.jpg").unwrap();
+        let img = execute(&img, 1920, 1080).unwrap();
+        assert_eq!(img.dimensions(), (1920, 1080));
+    }
+    #[test]
+    fn test_gray_tile() {
+        let img = DynamicImage::ImageLuma8(GrayImage::new(100, 100));
         let img = execute(&img, 1920, 1080).unwrap();
         assert_eq!(img.dimensions(), (1920, 1080));
     }
